f2c01f5e08264ca21cf823a1a911b6b82fe75a36
MalikDzh <944malik56@gamil.com>
2023-01-20 13:00:02 +0600

    studio_list

---
+++ src/components/Studios/StudioList.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { outContext } from '../Contexts/OutContext';
+import { Box } from '@mui/material'
+
+const StudioList = () => {
+    const { getStudios, studios, pages } = useContext(outContext);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    useEffect(() => {
+        getStudios();
+    }, []);
+
+    useEffect(() => {
+        getStudios();
+    }, [searchParams]);
+
+    useEffect(() => {
+        setSearchParams({
+            page: currentPage,
+        });
+    }, [currentPage]);
+
+    console.log(studios);
+
+    return (
+        <div>
+            <Box>
+                
+            </Box>
+        </div>
+    );
+};
+
+export default StudioList;
