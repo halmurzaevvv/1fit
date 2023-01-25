fe38614b13c31b77c3bc42af93b67f499c9981dc
Bayaraev <Joker0396@gmail.com>
2023-01-25 15:07:27 +0600

    scroll

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,26 +2,60 @@ import { MapContainer } from "react-leaflet/MapContainer";
 import { TileLayer } from "react-leaflet/TileLayer";
 import { useMap } from "react-leaflet/hooks";
 import "./Map.css";
-import { Marker, Popup } from "react-leaflet";
+import { LayersControl, Marker, Popup, ZoomControl } from "react-leaflet";
 import React from "react";
 import { Box } from "@mui/material";
 
 const Map = () => {
-  const position = [42.871636, 74.586767];
+  const position = [42.871413, 74.587144];
+
+  // {
+  //    AudioProcessingEvent.map((item) => (
+  //   <Pointer position={item.coordinates>
+  //     <Popup>
+  //       {item.title}
+  //       <Link to={`{API_PRODUCTS/details/${item.id}`}>
+  //       ПОДРОБНЕЕ
+  //       </Link>
+  //     </Popup>
+  //   }
+  //    ))
+  //   }
 
   return (
     <div>
       <Box>
         <MapContainer
-          center={position}
+          center={[42.871413, 74.587144]}
           zoom={13}
-          scrollWheelZoom={false}
+          scrollWheelZoom={true}
           className="map"
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <div className="map-btn">
+            <LayersControl
+              position="topright"
+              sx={{
+                mt: 5,
+              }}
+            >
+              <LayersControl.BaseLayer checked name="Basic Map">
+                <TileLayer
+                  attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+              </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer name="Topo Map">
+                <TileLayer
+                  attribution='Map data: &amp;copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &amp;copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+                  url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+                />
+              </LayersControl.BaseLayer>
+            </LayersControl>
+          </div>
           <Marker position={position}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
--- src/components/Studios/StudioList.jsx
+++ src/components/Studios/StudioList.jsx
@@ -47,33 +47,33 @@
 // };
 
 // export default StudioList;
-import React, { useContext, useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import React, { useContext, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 // import { outContext } from "../Contexts/OutContext"
 import {
-	Box,
-	CardActions,
-	CardContent,
-	CardMedia,
-	Container,
-	Typography,
-} from "@mui/material"
-import { alpha, styled, useTheme } from "@mui/material/styles"
-import OutlinedInput from "@mui/material/OutlinedInput"
-import InputLabel from "@mui/material/InputLabel"
-import MenuItem from "@mui/material/MenuItem"
-import FormControl from "@mui/material/FormControl"
-import Select from "@mui/material/Select"
-import AppBar from "@mui/material/AppBar"
-import Toolbar from "@mui/material/Toolbar"
-import IconButton from "@mui/material/IconButton"
-import InputBase from "@mui/material/InputBase"
-import MenuIcon from "@mui/icons-material/Menu"
-import SearchIcon from "@mui/icons-material/Search"
-import Map from "../Map/Map"
-import { Pagination } from "@mui/material"
-import { productContext } from "../Contexts/OutContext"
-import StudioCard from "./StudioCard"
+  Box,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Typography,
+} from "@mui/material";
+import { alpha, styled, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import Map from "../Map/Map";
+import { Pagination } from "@mui/material";
+import { productContext } from "../Contexts/OutContext";
+import StudioCard from "./StudioCard";
 
 // const { fetchByParams } = useContext(productContext);
 
@@ -87,276 +87,276 @@ import StudioCard from "./StudioCard"
 // }
 
 //!-------------------category
-const ITEM_HEIGHT = 48
-const ITEM_PADDING_TOP = 8
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
 const MenuProps = {
-	PaperProps: {
-		style: {
-			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-			width: 250,
-		},
-	},
-}
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 const names = [
-	"Oliver Hansen",
-	"Van Henry",
-	"April Tucker",
-	"Ralph Hubbard",
-	"Omar Alexander",
-]
+  "Oliver Hansen",
+  "Van Henry",
+  "April Tucker",
+  "Ralph Hubbard",
+  "Omar Alexander",
+];
 
 const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	"&:hover": {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginLeft: 0,
-	width: "100%",
-	[theme.breakpoints.up("sm")]: {
-		marginLeft: theme.spacing(1),
-		width: "auto",
-	},
-}))
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
 
 //!-------------------search
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-}))
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		width: "100%",
-		[theme.breakpoints.up("sm")]: {
-			width: "12ch",
-			"&:focus": {
-				width: "20ch",
-			},
-		},
-	},
-}))
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 function getStyles(name, personName, theme) {
-	return {
-		fontWeight:
-			personName.indexOf(name) === -1
-				? theme.typography.fontWeightRegular
-				: theme.typography.fontWeightMedium,
-	}
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
 }
 
 const StudioList = () => {
-	const theme = useTheme()
-	const [personName, setPersonName] = React.useState([])
+  const theme = useTheme();
+  const [personName, setPersonName] = React.useState([]);
 
-	const [searchParams, setSearchParams] = useSearchParams()
-	const [search, setSearch] = useState(searchParams.get("q") || "")
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState(searchParams.get("q") || "");
 
-	const { getProducts, products, pages } = useContext(productContext)
+  const { getProducts, products, pages } = useContext(productContext);
 
-	const [currentPage, setCurrentPage] = useState(1)
+  const [currentPage, setCurrentPage] = useState(1);
 
-	useEffect(() => {
-		getProducts()
-	}, [])
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-	useEffect(() => {
-		getProducts()
-	}, [searchParams])
+  useEffect(() => {
+    getProducts();
+  }, [searchParams]);
 
-	useEffect(() => {
-		setSearchParams({
-			page: currentPage,
-		})
-	}, [currentPage])
+  useEffect(() => {
+    setSearchParams({
+      page: currentPage,
+    });
+  }, [currentPage]);
 
-	console.log(products)
+  console.log(products);
 
-	useEffect(() => {
-		setSearchParams({
-			q: search,
-		})
-		console.log(searchParams.toString())
-	}, [search])
+  useEffect(() => {
+    setSearchParams({
+      q: search,
+    });
+    console.log(searchParams.toString());
+  }, [search]);
 
-	const handleChange = (event) => {
-		const {
-			target: { value },
-		} = event
-		setPersonName(
-			// On autofill we get a stringified value.
-			typeof value === "string" ? value.split(",") : value
-		)
-	}
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
 
-	// const { getStudios, studios, pages } = useContext(outContext)
-	// const [currentPage, setCurrentPage] = useState(1)
-	// const [searchParams, setSearchParams] = useSearchParams()
+  // const { getStudios, studios, pages } = useContext(outContext)
+  // const [currentPage, setCurrentPage] = useState(1)
+  // const [searchParams, setSearchParams] = useSearchParams()
 
-	// useEffect(() => {
-	// 	getStudios()
-	// }, [])
+  // useEffect(() => {
+  // 	getStudios()
+  // }, [])
 
-	// useEffect(() => {
-	// 	getStudios()
-	// }, [searchParams])
+  // useEffect(() => {
+  // 	getStudios()
+  // }, [searchParams])
 
-	// useEffect(() => {
-	// 	setSearchParams({
-	// 		page: currentPage,
-	// 	})
-	// }, [currentPage])
+  // useEffect(() => {
+  // 	setSearchParams({
+  // 		page: currentPage,
+  // 	})
+  // }, [currentPage])
 
-	// console.log(studios)
+  // console.log(studios)
 
-	return (
-		<Box
-			sx={{
-				// height: "100vh",
-				width: "100%",
-				display: "flex",
-				// alignItems: "center",
-				// justifyContent: "center",
-			}}
-		>
-			<Box
-				sx={{
-					pl: 5,
-					pr: 5,
-					width: "100%",
-					height: "180px",
-					background: "#fff",
-					zIndex: 6,
-					position: "fixed",
-					flexWrap: "wrap",
-				}}
-			>
-				<Box>
-					<Box
-						sx={{
-							mt: 5,
-						}}
-					>
-						<Typography
-							sx={{
-								mt: 50,
-								color: "#3447F6",
-								// width: "50%",
-								fontWeight: "700",
-							}}
-							// variant="h6"
-							component="span"
-						>
-							Главная{" "}
-						</Typography>
-						<Typography
-							sx={{
-								fontSize: "13px",
-							}}
-							component="span"
-							color="grey"
-						>
-							/ Залы и студии
-						</Typography>
-					</Box>
-					<Box>
-						<Typography
-							sx={{
-								mt: 0,
-								// width: "50%",
-								fontWeight: "700",
-								fontSize: "40px	",
-							}}
-							component="div"
-						>
-							Залы и студии{" "}
-						</Typography>
-					</Box>
-				</Box>
-				<Box
-					sx={{
-						width: "100%",
-						mb: 500,
-					}}
-				>
-					<Toolbar component="div" sx={{ flexWrap: "wrap" }}>
-						<Box
-							sx={{
-								width: "20%",
-							}}
-						>
-							<Search
-								sx={{
-									border: "1px solid grey",
-									borderRadius: "25px",
-								}}
-							>
-								<SearchIconWrapper>
-									<SearchIcon sx={{ color: "grey" }} />
-								</SearchIconWrapper>
-								<StyledInputBase
-									placeholder="Поиск…"
-									inputProps={{ "aria-label": "search" }}
-								/>
-							</Search>
-						</Box>
-						<FormControl sx={{ m: 1, width: 250 }}>
-							<InputLabel sx={{ mt: -1 }} id="demo-multiple-name-label">
-								Категории
-							</InputLabel>
-							<Select
-								sx={{
-									borderRadius: "25px",
-									height: "40px",
-								}}
-								labelId="demo-multiple-name-label"
-								id="demo-multiple-name"
-								multiple
-								value={personName}
-								onChange={handleChange}
-								input={<OutlinedInput label="Name" />}
-								MenuProps={MenuProps}
-							>
-								{names.map((name) => (
-									<MenuItem
-										key={name}
-										value={name}
-										style={getStyles(name, personName, theme)}
-									>
-										{name}
-									</MenuItem>
-								))}
-							</Select>
-						</FormControl>
-					</Toolbar>
-				</Box>
-			</Box>
+  return (
+    <Box
+      sx={{
+        // height: "100vh",
+        width: "100%",
+        display: "flex",
+        // alignItems: "center",
+        // justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          pl: 5,
+          pr: 5,
+          width: "100%",
+          height: "180px",
+          background: "#fff",
+          zIndex: 6,
+          position: "fixed",
+          flexWrap: "wrap",
+        }}
+      >
+        <Box>
+          <Box
+            sx={{
+              mt: 5,
+            }}
+          >
+            <Typography
+              sx={{
+                mt: 50,
+                color: "#3447F6",
+                // width: "50%",
+                fontWeight: "700",
+              }}
+              // variant="h6"
+              component="span"
+            >
+              Главная{" "}
+            </Typography>
+            <Typography
+              sx={{
+                fontSize: "13px",
+              }}
+              component="span"
+              color="grey"
+            >
+              / Залы и студии
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              sx={{
+                mt: 0,
+                // width: "50%",
+                fontWeight: "700",
+                fontSize: "40px	",
+              }}
+              component="div"
+            >
+              Залы и студии{" "}
+            </Typography>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            mb: 500,
+          }}
+        >
+          <Toolbar component="div" sx={{ flexWrap: "wrap" }}>
+            <Box
+              sx={{
+                width: "20%",
+              }}
+            >
+              <Search
+                sx={{
+                  border: "1px solid grey",
+                  borderRadius: "25px",
+                }}
+              >
+                <SearchIconWrapper>
+                  <SearchIcon sx={{ color: "grey" }} />
+                </SearchIconWrapper>
+                <StyledInputBase
+                  placeholder="Поиск…"
+                  inputProps={{ "aria-label": "search" }}
+                />
+              </Search>
+            </Box>
+            <FormControl sx={{ m: 1, width: 250 }}>
+              <InputLabel sx={{ mt: -1 }} id="demo-multiple-name-label">
+                Категории
+              </InputLabel>
+              <Select
+                sx={{
+                  borderRadius: "25px",
+                  height: "40px",
+                }}
+                labelId="demo-multiple-name-label"
+                id="demo-multiple-name"
+                multiple
+                value={personName}
+                onChange={handleChange}
+                input={<OutlinedInput label="Name" />}
+                MenuProps={MenuProps}
+              >
+                {names.map((name) => (
+                  <MenuItem
+                    key={name}
+                    value={name}
+                    style={getStyles(name, personName, theme)}
+                  >
+                    {name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Toolbar>
+        </Box>
+      </Box>
 
-			<Box
-				sx={{
-					mt: 10,
-					width: "100%",
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					zIndex: 2,
-				}}
-			>
-				{/* --------------------StudioCart------------------------- */}
-				{/* <Box
+      <Box
+        sx={{
+          mt: 10,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          zIndex: 2,
+        }}
+      >
+        {/* --------------------StudioCart------------------------- */}
+        {/* <Box
 					sx={{
 						// alignItems: "center",
 						width: "60%",
@@ -421,46 +421,48 @@ const StudioList = () => {
 					</Box>
 				</Box> */}
 
-				<Box>
-					ProductList
-					{products.map((item) => (
-						<StudioCard key={item.id} item={item} />
-					))}
-				</Box>
-				<Box>
-					<Pagination
-						count={pages}
-						variant="outlined"
-						color="primary"
-						page={currentPage}
-						onChange={(e, p) => setCurrentPage(p)}
-					/>
-				</Box>
+        <Box>
+          ProductList
+          {products.map((item) => (
+            <StudioCard key={item.id} item={item} />
+          ))}
+        </Box>
+        <Box>
+          <Pagination
+            count={pages}
+            variant="outlined"
+            color="primary"
+            page={currentPage}
+            onChange={(e, p) => setCurrentPage(p)}
+          />
+        </Box>
 
-				{/* --------------------StudioCart end------------------------- */}
+        {/* --------------------StudioCart end------------------------- */}
 
-				{/* //!================================Map start=================================== */}
-				<Box
-					sx={{
-						width: "50%",
-						height: "100%",
-						backgroundColor: "lightcyan",
-						// position: "sticky",
-						// display: "flex",
-						// justifyContent: "center",
-						// alignItems: "center",
-						// bottom: "100px",
-						// top: "200px",
-					}}
-				>
-					<Map />
-				</Box>
-				{/* //!================================Map end=================================== */}
-			</Box>
-		</Box>
-	)
-}
+        {/* //!================================Map start=================================== */}
+        <Box
+          sx={{
+            width: "60%",
+            height: "480px",
+            // display: "flex",
+            // backgroundColor: "white",
+            position: "sticky",
+            // display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap: "wrap",
+            bottom: "20px",
+            // top: "200px",
+          }}
+        >
+          <Map />
+        </Box>
+        {/* //!================================Map end=================================== */}
+      </Box>
+    </Box>
+  );
+};
 
-export default StudioList
+export default StudioList;
 
 // Funky Dance Center - это не просто танцевальный центр, это одно из тех немногих мест, где раскрывают таланты. И занимаются здесь раскрытием Ваших тала...
